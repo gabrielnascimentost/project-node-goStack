232efafa931b89fc2c91bf0eccb26d12ae3290fc
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-04-14 16:43:10 -0300

    criando relacao entre tabelas do bd

--- src/database/migrations/1618240933124-CreateAppointments.ts
+++ src/database/migrations/1618240933124-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1618240933124
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
