91c901eb112b48e6ae848eb0f2b6043a912cd44d
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-04-16 19:14:01 -0300

    camada de seguranca p/ avatar e upload de files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
     try {
@@ -21,5 +27,27 @@ usersRouter.post('/', async (req, res) => {
         return res.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+            const user = await updateUserAvatar.execute({
+                user_id: req.user.id,
+                avatarFilename: req.file.filename,
+            });
+
+            // @ts-expect-error ignorando erro de interface
+            delete user.password;
+
+            return res.json(user);
+        } catch (err) {
+            return res.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default usersRouter;
