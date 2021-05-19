b0fa9626fab3dacc9ae8ddd9dc263857b5d52344
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-05-19 19:04:24 -0300

    providers controllers

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -12,5 +12,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/providers', profileRouter);
 
 export default routes;
