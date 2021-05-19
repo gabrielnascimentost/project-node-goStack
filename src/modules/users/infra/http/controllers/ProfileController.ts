8c10be3b0765fdfe8f90f11ec91c3b7a62b0fa5a
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-05-19 12:05:03 -0300

    service show profile

---
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,36 @@
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class ProfileController {
+    public async show(request: Request, response: Response): Promise<Response> {
+        const { id: user_id } = request.user;
+        const showProfile = container.resolve(ShowProfileService);
+        const user = await showProfile.execute({ user_id });
+        return response.status(200).json(user);
+    }
+
+    public async update(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { id: user_id } = request.user;
+
+        const { name, email, old_password, password } = request.body;
+
+        const updateProfile = container.resolve(UpdateProfileService);
+
+        const user = await updateProfile.execute({
+            user_id,
+            name,
+            email,
+            old_password,
+            password,
+        });
+        delete user.password;
+        return response.json(user);
+    }
+}
+
+export default ProfileController;
