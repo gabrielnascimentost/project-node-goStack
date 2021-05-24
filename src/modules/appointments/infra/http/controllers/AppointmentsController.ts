57ca305500e6e1d98c38ad9fceecb05edb091f0b
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-05-24 16:25:28 -0300

    bug fix

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
+
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -12,10 +12,10 @@ export default class AppointmentsController {
     ): Promise<Response> {
         const user_id = request.user.id;
         const { provider_id, date } = request.body;
-        const parseDate = parseISO(date);
+
         const createAppointment = container.resolve(CreateAppointmentService);
         const appointment = await createAppointment.execute({
-            date: parseDate,
+            date,
             provider_id,
             user_id,
         });
